6d7c583fb0c1ac1feb9624bce04674a180e71441
Cesar Alcancio <cesar.alcancio@payclip.com>
2021-01-27 11:58:48 -0600

    first commit

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject rest-demo "0.1.0-SNAPSHOT"
+  :description "Rest Demo Using the https://medium.com/swlh/building-a-rest-api-in-clojure-3a1e1ae096e"
+  :url "Rest Demo https://medium.com/swlh/building-a-rest-api-in-clojure-3a1e1ae096e"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [
+                 [org.clojure/clojure "1.10.0"]
+                 ; Compojure - A basic routing library
+                 [compojure "1.6.1"]
+                 ; Our Http library for client/server
+                 [http-kit "2.3.0"]
+                 ; Ring defaults - for query params etc
+                 [ring/ring-defaults "0.3.2"]
+                 ; Clojure data.JSON library
+                 [org.clojure/data.json "0.2.6"]
+                 ]
+  :main ^:skip-aot rest-demo.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
