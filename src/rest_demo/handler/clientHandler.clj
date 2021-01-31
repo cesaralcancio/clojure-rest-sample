cf47729d163d8845b1de8c9f7c21b13304f82904
Cesar Alcancio <cesar.alcancio@payclip.com>
2021-01-30 22:38:52 -0600

    Add uuid as parameter for POST client

--- src/rest_demo/handler/clientHandler.clj
+++ src/rest_demo/handler/clientHandler.clj
@@ -2,13 +2,23 @@
   (:require [clojure.pprint :as pp]
             [ring.util.request :as r.u.request]
             [cheshire.core :as cheshire]
+            [clojure.string :as s]
             [rest-demo.model.clientModel :as client.model]))
+
+; (def smile (cheshire/generate-smile {:foo "bar" :baz 5}))
+; (cheshire/parse-smile smile)
+; (cheshire/generate-string {:foo "bar" :baz 5})
 
 (defn clients-handler [req]
   {:status  200
    :headers {"Content-type" "text/json"}
    :body    (->> @client.model/client-collection
                  cheshire/generate-string)})
+
+(defn apply-response [client]
+  (if-let [xpto (empty? client)]
+    {:message "There's no client with this uuid"}
+    client))
 
 (defn client-handler [req]
   {:status  200
@@ -17,6 +27,7 @@
               (->> @client.model/client-collection
                    (filter #(= (:uuid %) uuid))
                    first
+                   apply-response
                    cheshire/generate-string))})
 
 (defn addclient-handler [req]
@@ -24,11 +35,17 @@
    :headers {"Content-Type" "text/json"}
    :body    (let [str-json (r.u.request/body-string req)
                   map-json (cheshire/parse-string str-json true)
+                  uuid (:uuid map-json)
                   firstname (:firstname map-json)
                   surname (:surname map-json)
                   email (:email map-json)
                   birthday (:birthday map-json)
-                  response (client.model/add-client! firstname surname email birthday)]
+                  response (client.model/add-client!
+                             uuid
+                             firstname
+                             surname
+                             email
+                             birthday)]
               (cheshire/generate-string response))})
 
 (defn remove-client-handler [req]
--- src/rest_demo/model/clientModel.clj
+++ src/rest_demo/model/clientModel.clj
@@ -1,25 +1,43 @@
 (ns rest-demo.model.clientModel
-  (:require [clojure.pprint :as pp]))
+  (:require [clojure.pprint :as pp]
+            [clojure.string :as s]))
 
 (def client-collection (atom []))
 
-(defn add-client! [firstname surname email birthdate]
-  (let [new-client {:uuid       (str (java.util.UUID/randomUUID))
-                    :firstname  firstname
-                    :surname    surname
-                    :email      email
-                    :birthday   birthdate
-                    :created_at (new java.util.Date)}]
-    (swap! client-collection conj new-client)
-    new-client))
+(defn uuid-gen [] (java.util.UUID/randomUUID))
+
+(defn date-gen [] (new java.util.Date))
+
+(defn valida-uuid [uuid]
+  (if-let [validated-uuid (or (nil? uuid) (s/blank? uuid))]
+    (uuid-gen) uuid))
+(valida-uuid nil)
+(valida-uuid "")
+(valida-uuid "123")
+
+(defn add-client!
+  ([firstname surname email birthday]
+   (add-client! (str (uuid-gen))
+                firstname
+                surname
+                email
+                birthday))
+  ([uuid firstname surname email birthday]
+   (let [new-client {:uuid       (valida-uuid uuid)
+                     :firstname  firstname
+                     :surname    surname
+                     :email      email
+                     :birthday   birthday
+                     :created_at (date-gen)}]
+     (swap! client-collection conj new-client)
+     new-client)))
 
 (defn remove-client [collection uuid]
   (remove #(= (:uuid %) uuid) collection))
 
 (defn remove-client! [uuid]
-  (swap! client-collection remove-client uuid)
-  )
-(remove-client! "f027ea34-f061-44a9-bd5e-c9599323051e")
+  (swap! client-collection remove-client uuid))
+(remove-client! "123")
 
-(add-client! "Jose" "Antonio" "jose@gmail.com" "1991-01-01")
+(add-client! "123" "Jose" "Antonio" "jose@gmail.com" "1991-01-01")
 (pp/pprint client-collection)
